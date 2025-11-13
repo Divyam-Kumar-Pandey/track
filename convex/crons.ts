6cd7f3a2ffc285c5d0e512b90acefe9a68b17293
Divyam Kumar Pandey <divyamkumarp@gmail.com>
2025-11-14 04:25:35 +0530

    Update cron job schedule for daily report generation

--- convex/crons.ts
+++ convex/crons.ts
@@ -4,10 +4,9 @@ import dayjs from "dayjs";
 
 const crons = cronJobs();
 
-// An alternative way to create the same schedule as above with cron syntax
 crons.cron(
   "create daily report",
-  "0 5 0 * *",
+  "5 0 * * *",
   api.daily_report.createDailyReport,
   { date: dayjs().format("YYYY-MM-DD") },
 );
